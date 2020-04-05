2dac563e3d6ce8e00d3d125df25963f4394ba430
pepp97 <giuseppe1.asaro@mail.polimi.it>
2020-04-05 23:51:16 +0200

    fix

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,12 @@ module AM06 {
     requires javafx.controls;
     requires com.fasterxml.jackson.core;
     requires com.fasterxml.jackson.databind;
+    requires java.base;
     exports it.polimi.ingsw.view;
     exports it.polimi.ingsw.model;
+    exports it.polimi.ingsw.ParserClient;
+    exports it.polimi.ingsw.ParserServer;
+    exports it.polimi.ingsw.events;
+    exports it.polimi.ingsw.controller;
+    exports it.polimi.ingsw.commands;
 }
