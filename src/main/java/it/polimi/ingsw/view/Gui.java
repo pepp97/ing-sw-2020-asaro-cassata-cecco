e15c2fb6df679e9d391eca05870616eaec2db5db
RiccardoCecco <10572767@polimi.it>
2020-05-01 23:58:53 +0200

    new graphics

--- src/main/java/it/polimi/ingsw/view/Gui.java
+++ src/main/java/it/polimi/ingsw/view/Gui.java
@@ -9,8 +9,10 @@ import it.polimi.ingsw.model.Player;
 import it.polimi.ingsw.view.gui.*;
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.geometry.VPos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -18,6 +20,8 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.text.*;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -99,8 +103,152 @@ public class Gui extends  Application implements View {
         });
         Scene startMenu;
 
-        exitGame = new SelectBox("Vuoi uscire dal gioco?");
-        GridPane menu = new GridPane();
+
+        exitGame = new SelectBox("Do you really want to exit??");
+
+
+        boolean continua;
+
+        StackPane stackPane = new StackPane();
+        startMenu = new Scene(stackPane,widthScreen,heightScreen );
+
+        GridPane structure = new GridPane();
+        RowConstraints l1 = new RowConstraints();
+        l1.setValignment(VPos.CENTER);
+        RowConstraints l2 = new RowConstraints();
+        structure.getRowConstraints().addAll(l1, l2);
+
+        structure.setStyle("-fx-alignment: top-center;");
+
+        StackPane top= new StackPane();
+        top.setStyle("-fx-background-color: #000000;");
+        StackPane bottom= new StackPane();
+
+        structure.add(top,0,0);
+        structure.add(bottom,0,1);
+
+
+        Image intro = new Image("SantoriniIntro.jpg", widthScreen,heightScreen,true, true); //modificare percorso.
+        ImageView imageView = new ImageView();
+        imageView.setImage(intro);
+        stackPane.getChildren().add(structure);
+        BackgroundImage backgroundImage = new BackgroundImage(intro, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, BackgroundSize.DEFAULT);
+        structure.setBackground(new Background(backgroundImage));
+
+
+        Text testo = new Text("CONNECTION");
+        testo.setFill(javafx.scene.paint.Color.BLACK);
+        testo.setFont(Font.font("Helvetica", FontWeight.BOLD, 25));
+        TextFlow title = new TextFlow(testo);
+        title.setMaxHeight(60);
+        title.setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
+        title.setTextAlignment(TextAlignment.CENTER);
+
+
+
+        top.getChildren().add(title);
+        top.setMinWidth(structure.getLayoutX());
+        top.setStyle("-fx-alignment: center; -fx-border-color: black;");
+        top.setAlignment(title,Pos.CENTER);
+
+
+        GridPane menu=new GridPane();
+        menu.setMinWidth(250);
+        menu.setMinHeight(180);
+        RowConstraints r1 = new RowConstraints();
+        r1.setMinHeight(30);
+        RowConstraints r2 = new RowConstraints();
+        r2.setMinHeight(30);
+        RowConstraints r3 = new RowConstraints();
+        r3.setMinHeight(30);
+        RowConstraints r4 = new RowConstraints();
+        r3.setMinHeight(30);
+        menu.getRowConstraints().addAll(r1, r2,r3,r4);
+
+        menu.setStyle("-fx-alignment: center;  -fx-border-color: black;");
+
+        TextField ipInput = new TextField("localhost");
+        ipInput.setPromptText("IP Default: localhost");
+        menu.add(ipInput,0,0);
+
+        TextField portInput = new TextField("8080");
+        portInput.setPromptText("Default port:8080");
+        menu.add(portInput,0,1);
+
+
+        GridPane bottomGrid=new GridPane();
+        bottomGrid.setMinWidth(150);
+        bottomGrid.setMinHeight(60);
+        ColumnConstraints c11= new ColumnConstraints();
+        c11.setMinWidth(120);
+        ColumnConstraints c22= new ColumnConstraints();
+        c22.setMinWidth(120);
+        c11.setHalignment(HPos.LEFT);
+        c22.setHalignment(HPos.RIGHT);
+        bottomGrid.getColumnConstraints().addAll(c11,c22);
+
+
+
+
+
+        Button connectionButton = new Button("Vai al Login");
+        connectionButton.setStyle("-fx-alignment: center;");
+        String styleLogin="-fx-text-fill: #ffffff;" +"-fx-background-color: #000000;"+"-fx-font-size: 12pt;"+"-fx-font-weight: bold;"+"-fx-border-radius: 20;"+"-fx-background-radius: 20;";
+        connectionButton.setStyle(styleLogin);
+        StackPane left=new StackPane();
+        left.setStyle("-fx-alignment: center;");
+        left.getChildren().add(connectionButton);
+        left.setAlignment(connectionButton,Pos.CENTER);
+        bottomGrid.add(left,0,0);
+
+        Button close = new Button("exit");
+        String styleExit="-fx-text-fill: #000000;" +"-fx-background-color: #ff0000;"+"-fx-font-size: 12pt;"+"-fx-font-weight: bold;"+"-fx-border-radius: 20;"+"-fx-background-radius: 20;";
+        close.setStyle(styleExit);
+        StackPane right=new StackPane();
+        right.setStyle("-fx-alignment: center;");
+        right.getChildren().add(close);
+        right.setAlignment(close,Pos.CENTER);
+        bottomGrid.add(right,1,0);
+
+        menu.add(bottomGrid,0,2);
+
+        structure.add(menu,0,1);
+
+        primaryStage.setScene(startMenu);
+        Stage second = new Stage();
+        second.initOwner(primaryStage);
+
+        primaryStage.show();
+
+        connectionButton.setOnAction(e -> {
+                    if (ipInput.getText().equals("") || portInput.getText().equals("")) {
+                        Label error = new Label("COMPLETE THE FIELDS!!");
+                        String styleError="-fx-alignment: center;-fx-font-size: 14pt;" + "-fx-font-family: \"Segoe UI Semibold\";" + "-fx-text-fill: red;"+"-fx-opacity: 1;";
+                        error.setStyle(styleError);
+
+                        menu.add(error,0,3);
+
+                    } else {
+                        try {
+                            client = new Client(ipInput.getText(), Integer.decode(portInput.getText()),this);
+                            client.start();
+                            Connection command=new Connection();
+                            client.send(command);
+
+
+                        } catch (Exception e1) {
+                            e1.printStackTrace();
+                        }
+                    }
+                }
+
+        );
+
+
+    }
+
+    /*
+    GridPane menu = new GridPane();
         menu.setPadding(new Insets(50, 50, 50, 50));
         menu.setHgap(1);
         menu.setVgap(1);
@@ -124,6 +272,8 @@ public class Gui extends  Application implements View {
         menu.setBackground(new Background(backgroundImage));
         continua = false;
 
+
+
         menu.setAlignment(Pos.CENTER);
         primaryStage.setScene(startMenu);
         Stage second = new Stage();
@@ -131,6 +281,8 @@ public class Gui extends  Application implements View {
         TextField ipInput = new TextField("localhost");
         ipInput.setPromptText("ip default: localhost");
 
+        String style="-fx-border-color: black; -fx-background-color:white;";
+        GridPane.setStyle();
         GridPane.setConstraints(ipInput, 150, 120);
 
 
@@ -142,7 +294,7 @@ public class Gui extends  Application implements View {
         primaryStage.show();
         connectionButton.setOnAction(e -> {
                     if (ipInput.getText().equals("") || portInput.getText().equals("")) {
-                        Label error = new Label("        Completa i campi errati, scemo");
+                        Label error = new Label("        Completa i campi errati");
                         GridPane.setConstraints(error, 150, 80);
                         menu.getChildren().add(0, error);
                     } else {
@@ -166,8 +318,7 @@ public class Gui extends  Application implements View {
         menu.getChildren().addAll(connectionButton, close, ipInput, portInput);
         // close.setOnAction(e -> closeApplication());
 
-
-    }
+     */
 
     private void closeApplication() {
 
