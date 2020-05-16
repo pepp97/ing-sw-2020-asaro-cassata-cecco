7362f452d606b475dfbf5410d85572d12b180700
pepp97 <giuseppe1.asaro@mail.polimi.it>
2020-05-16 18:39:06 +0200

    fix bugs

--- src/main/java/it/polimi/ingsw/controller/ExecuteRoutineState.java
+++ src/main/java/it/polimi/ingsw/controller/ExecuteRoutineState.java
@@ -3,6 +3,7 @@ package it.polimi.ingsw.controller;
 import it.polimi.ingsw.ParserServer.SquareToJson;
 import it.polimi.ingsw.commands.ChooseYourWorker;
 import it.polimi.ingsw.events.ChooseWorker;
+import it.polimi.ingsw.model.Move;
 import it.polimi.ingsw.model.Square;
 import it.polimi.ingsw.model.Worker;
 
@@ -18,30 +19,13 @@ public class ExecuteRoutineState implements TurnState {
     @Override
     public void executeState(Controller controller) {
         this.controller = controller;
-        if (controller.getGame().getWinner() != null || controller.getGame().getCurrentPlayer().isDefeat())
-            return;
+
         System.out.println("round: " + i);
+        if (controller.getGame().getWinner() != null)
+            return;
+
         if (i == -1) {
-            List<Square> pos = new ArrayList<>();
-            for (Worker s : controller.getGame().getCurrentPlayer().getWorkers())
-                pos.add(s.getSquare());
-            List<SquareToJson> positions = new ArrayList<>();
-            for (Square s : pos)
-                positions.add(new SquareToJson(s.getLevel(), s.getWorker().getC().toString(), s.getCoordinateX(), s.getCoordinateY()));
-            SquareToJson[][] map = new SquareToJson[5][5];
-            Square[][] mappa = controller.getGame().getField().getSquares();
-
-            for (int x = 0; x < 5; x++)
-                for (int y = 0; y < 5; y++)
-                    if (mappa[x][y].getWorker() != null)
-                        map[x][y] = new SquareToJson(mappa[x][y].getLevel(), mappa[x][y].getWorker().getC().toString(), x, y);
-                    else map[x][y] = new SquareToJson(mappa[x][y].getLevel(), "", x, y);
-            ChooseWorker chooseWorker = new ChooseWorker(positions, map);
-            controller.getGame().getCurrentPlayer().setInQue(true);
-            controller.getGame().notifyCurrent(chooseWorker);
-            i++;
-            controller.setGoOn(false);
-
+            chooseWorker();
             return;
 
         } else {
@@ -49,8 +33,8 @@ public class ExecuteRoutineState implements TurnState {
             if (controller.getGame().getCurrentPlayer().isInQue())
                 return;
 
-            if (i < controller.getGame().getCurrentPlayer().getGod().getRoutine().size()) {
-                if ((!controller.isCanSkip()) || (!controller.getGame().getCurrentPlayer().getGod().getRoutine().get(i).isSkippable())) {
+            if (!controller.getGame().getCurrentPlayer().isDefeat()&&i < controller.getGame().getCurrentPlayer().getGod().getRoutine().size()) {
+                if (((!controller.isCanSkip()) || (!controller.getGame().getCurrentPlayer().getGod().getRoutine().get(i).isSkippable()))) {
                     if (!controller.isGoOn()) {
 
                         result = controller.getGame().getCurrentPlayer().getGod().getRoutine().get(i).getEffect().isUsable(controller.getGame());
@@ -62,11 +46,18 @@ public class ExecuteRoutineState implements TurnState {
                     }
                     else controller.setGoOn(false);
                 }
-            } else if (!controller.getGame().getCurrentPlayer().isHasBuilt() && i >= controller.getGame().getCurrentPlayer().getGod().getRoutine().size()) {
+            } else if ((!controller.getGame().getCurrentPlayer().isHasBuilt() && i <= controller.getGame().getCurrentPlayer().getGod().getRoutine().size()) || controller.getGame().getCurrentPlayer().isDefeat() ){
                 controller.getGame().getCurrentPlayer().setDefeat(true);
                 TurnState state = new DefeatState();
                 controller.setState(state);
                 state.executeState(controller);
+                state = new StartTurnState();
+                i = -1;
+                controller.setState(state);
+                state.executeState(controller);
+                ExecuteRoutineState state1 = new ExecuteRoutineState();
+                controller.setState(state1);
+                state1.executeState(controller);
             } else {
                 TurnState state = new StartTurnState();
                 i = -1;
@@ -88,6 +79,29 @@ public class ExecuteRoutineState implements TurnState {
             executeState(controller);
         else if (controller.getGame().getCurrentPlayer().getGod().getRoutine().get(i).getEffect().isInterationNeeded())
             executeState(controller);
+
+    }
+
+    private void chooseWorker() {
+        List<Square> pos = new ArrayList<>();
+        for (Worker s : controller.getGame().getCurrentPlayer().getWorkers())
+            pos.add(s.getSquare());
+        List<SquareToJson> positions = new ArrayList<>();
+        for (Square s : pos)
+            positions.add(new SquareToJson(s.getLevel(), s.getWorker().getC().toString(), s.getCoordinateX(), s.getCoordinateY()));
+        SquareToJson[][] map = new SquareToJson[5][5];
+        Square[][] mappa = controller.getGame().getField().getSquares();
+
+        for (int x = 0; x < 5; x++)
+            for (int y = 0; y < 5; y++)
+                if (mappa[x][y].getWorker() != null)
+                    map[x][y] = new SquareToJson(mappa[x][y].getLevel(), mappa[x][y].getWorker().getC().toString(), x, y);
+                else map[x][y] = new SquareToJson(mappa[x][y].getLevel(), "", x, y);
+        ChooseWorker chooseWorker = new ChooseWorker(positions, map);
+        controller.getGame().getCurrentPlayer().setInQue(true);
+        controller.getGame().notifyCurrent(chooseWorker);
+        i++;
+        controller.setGoOn(false);
 
     }
 
