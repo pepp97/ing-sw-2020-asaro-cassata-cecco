051782bc505d1c6ad22d819dfb8514e46eed1719
pepp97 <giuseppe1.asaro@mail.polimi.it>
2020-06-08 09:58:21 +0200

    add class

---
+++ src/main/java/it/polimi/ingsw/commands/PingDelete.java
@@ -0,0 +1,13 @@
+package it.polimi.ingsw.commands;
+
+import it.polimi.ingsw.commands.Command;
+import it.polimi.ingsw.commands.LoginCommand;
+import it.polimi.ingsw.controller.Controller;
+import it.polimi.ingsw.view.VirtualView;
+
+public class PingDelete implements Command {
+    @Override
+    public void execute(Controller controller, VirtualView view) {
+        controller.apply(this, view);
+    }
+}
